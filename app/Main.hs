deec85cafb64a76aefc6cfdfe6c493ea4d0bea99
ben lim <lim6112j@gmail.com>
2021-07-14 03:56:57 +0900

    merge parser with applicative way

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,8 @@ module Main where
 import Options.Applicative
 
 type ItemIndex = Int
+data Options = Options FilePath ItemIndex deriving Show
+
 defaultDataPath :: FilePath
 defaultDataPath = "./to-do.yaml"
 dataPathParser :: Parser FilePath
@@ -14,10 +16,14 @@ dataPathParser = strOption $
   <> help ("path to data file (default " ++ defaultDataPath ++ ")")
 itemIndexParser :: Parser ItemIndex
 itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+optionsParser :: Parser Options
+optionsParser = Options <$> dataPathParser <*> itemIndexParser
 main :: IO ()
 main = do
-  dataPath <- execParser (info (dataPathParser) (progDesc "ToDo list file path"))
-  putStrLn $ "dataPath = " ++ show dataPath
+  options <- execParser (info (optionsParser) (progDesc "Todo list Manager"))
+  putStrLn $ "options = " ++ show options
+ -- dataPath <- execParser (info (dataPathParser) (progDesc "ToDo list file path"))
+  --putStrLn $ "dataPath = " ++ show dataPath
   --itemIndex <- execParser (info (itemIndexParser) (progDesc "To do list program"))
   --putStrLn $ "itemindex =" ++ show itemIndex
 
